528bbdf4d62091e9a2ef3869b8b3a393bf2a79a5
TigDav <tigdav2004@inbox.ru>
2023-08-03 16:37:53 +0300

    TeamCore v1.1

---
+++ src/pocketmine/network/protocol/DataPacket.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @author uzyandev
+ * @link https://github.com/uzyandev
+ * @link https://vk.com/dima.uzyanov
+ */
+
+namespace pocketmine\network\protocol;
+
+use pocketmine\network\mcpe\protocol\DataPacket as DataPacketFixNamespace;
+
+abstract class DataPacket extends DataPacketFixNamespace
+{}
